123fa1613019c8a05b07049bc449a84ed7bb3ff1
Mahfuzur Rahman <mahfuzurrahman0012@gmail.com>
2025-12-17 16:43:46 +0600

    update

--- src/Routes/Router.jsx
+++ src/Routes/Router.jsx
@@ -33,7 +33,7 @@ export const router = createBrowserRouter([
   {
     path: "/",
     element: <Root />,
-    // errorElement: <Error />,
+    errorElement: <Error />,
     children: [
       {
         path: "/",
